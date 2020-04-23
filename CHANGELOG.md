3316463f120f539724936f7bc88e4f16d49fc963
Vivien <vdebona@umanit.fr>
2020-04-23 09:25:36 +0200

    Add "['is_safe' => ['html']]" on functions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- Add `['is_safe' => ['html']]` on functions
 
 ## 1.0.0 - 2020-04-14
 First release 🎉
--- src/Extension.php
+++ src/Extension.php
@@ -18,11 +18,31 @@ class Extension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('umanit_image_figure', [Runtime::class, 'getUmanitImageFigure']),
-            new TwigFunction('umanit_image_figure_lazy_load', [Runtime::class, 'getUmanitImageFigureLazyLoad']),
-            new TwigFunction('umanit_image_picture', [Runtime::class, 'getUmanitImagePicture']),
-            new TwigFunction('umanit_image_picture_lazy_load', [Runtime::class, 'getUmanitImagePictureLazyLoad']),
-            new TwigFunction('umanit_image_srcset', [Runtime::class, 'getUmanitImageSrcset']),
+            new TwigFunction(
+                'umanit_image_figure',
+                [Runtime::class, 'getUmanitImageFigure'],
+                ['is_safe' => ['html']]
+            ),
+            new TwigFunction(
+                'umanit_image_figure_lazy_load',
+                [Runtime::class, 'getUmanitImageFigureLazyLoad'],
+                ['is_safe' => ['html']]
+            ),
+            new TwigFunction(
+                'umanit_image_picture',
+                [Runtime::class, 'getUmanitImagePicture'],
+                ['is_safe' => ['html']]
+            ),
+            new TwigFunction(
+                'umanit_image_picture_lazy_load',
+                [Runtime::class, 'getUmanitImagePictureLazyLoad'],
+                ['is_safe' => ['html']]
+            ),
+            new TwigFunction(
+                'umanit_image_srcset',
+                [Runtime::class, 'getUmanitImageSrcset'],
+                ['is_safe' => ['html']]
+            ),
         ];
     }
 }
